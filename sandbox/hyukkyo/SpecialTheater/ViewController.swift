8bd79a6d0c0f9ede068d2d23e5c557e1cb4fd3df
hyeokgyo <hyeokgyo@hyeokgyoui-MacBookPro.local>
2022-05-23 00:46:56 +0900

    add image and title in start screen

--- sandbox/hyukkyo/SpecialTheater/ViewController.swift
+++ sandbox/hyukkyo/SpecialTheater/ViewController.swift
@@ -7,22 +7,12 @@
 
 import UIKit
 
-struct MovieData : Codable {
-    let boxOfficeResult : BoxOfficeResult
-}
-struct BoxOfficeResult : Codable {
-    let dailyBoxOfficeList : [DailyBoxOfficeList]
-}
-struct DailyBoxOfficeList : Codable {
-    let movieNm : String
-}
-
 class ViewController: UIViewController {
+    
+    var movies: [NSDictionary]?
+    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
-    
-    var movieURL = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=7075174d390d9574c21e3da53249cfd1&targetDt="
-    
     
     // MARK: - Life Cycles
     override func viewDidLoad() {
@@ -34,30 +24,38 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         
-        movieURL += makeYesterdayString()
+        fetchMovies()
         
         // 사용할 컬렉션뷰 셀을 등록합니다.
         let movieItemNib = UINib(nibName: String(describing: movieItem.self), bundle: nil)
         self.collectionView.register(movieItemNib, forCellWithReuseIdentifier: String(describing: movieItem.self))
         self.collectionView.collectionViewLayout = createCompositionalLayout()
-        
-        getData()
-    }
-    
-    
-    func makeYesterdayString() -> String {
-        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyyMMdd"
-
-        let result = dateFormatter.string(from: yesterday!)
-        return result
     }
     
     // 화면을 터치하면 검색을 위해 올라왔던 키보드가 다시 내려갑니다.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    func fetchMovies() {
+        let apiKey = "105965c4202d209cc72123519fa06719"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)&language=ko-KR")
+        let request = NSURLRequest(url: url! as URL, cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task: URLSessionDataTask = session.dataTask(with: request as URLRequest, completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print("response: \(responseDictionary)")
+                    self.movies = responseDictionary["results"] as! [NSDictionary]
+                    self.collectionView.reloadData()
+                }
+            }
+        })
+        task.resume()
+    }
+    
 }
 
 
@@ -85,13 +83,25 @@ extension ViewController {
 extension ViewController: UICollectionViewDataSource {
     // 컬렉션뷰에 표시할 셀의 개수를 리턴합니다.
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return movies?.count ?? 0
     }
     
     // indexPath번째에 표시할 셀을 리턴합니다.
     // movieItem 클래스에서 셀에 대한 설정이 이뤄집니다.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: movieItem.self), for: indexPath) as! movieItem
+        let movie = movies![indexPath.row] 
+        let title = movie["title"] as! String
+        let posterPath = movie["poster_path"] as! String
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        let imageUrl = baseUrl + posterPath
+        
+        cell.descLabel.text = title
+        if let data = try? Data(contentsOf: URL(string: imageUrl)!) {
+            cell.posterImage.image = UIImage(data: data)
+        }
+        
         return cell
     }
 }
